86eecc46b333895e769908ffd7c6d6b3f528f29c
Tom Webber <t.c.w.webber@gmail.com>
2026-02-01 19:59:23 +0100

    feat(cli): load variables from .env

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use color_eyre::eyre::Context as _;
+use dotenv::dotenv;
+
+type Result<T = ()> = color_eyre::Result<T>;
+
+fn env(var_name: &str) -> Result<String> {
+    env::var(var_name).with_context(|| {
+        format!("Failed to load {var_name} environment variable. Consider adding it to the `.env` file.")
+    })
+}
+
+fn main() -> Result {
+    color_eyre::install()?;
+    dotenv().context("Failed to load `.env` file. Please create it with the DOMAIN, USERNAME and PASSWORD variables.")?;
+    let _domain = env("DOMAIN");
+    let _username = env("USERNAME");
+    let _password = env("PASSWORD");
+    Ok(())
 }
